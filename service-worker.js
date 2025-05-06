5d7bc0e39ae5568ed1e900dba23cabf38a492196
CashWayAppRegulatedCentralBankOfAmerica <yvonneakinyiouma@gmail.com>
2025-05-06 21:53:31 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,10 +15,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting(); // Activate immediately
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
-})
+});
